3fa084a12dadc8e298243b8d164f7029562dca9c
Zakry27 <zak90078@gmail.com>
2024-05-08 08:03:27 -0700

    0x00. MySQL advanced

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Task 1: creates a table users
+-- The script can be executed on any database
+CREATE TABLE IF NOT EXISTS `users` (  
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255),
+    `country` ENUM('US', 'CO', 'TN') NOT NULL 
+)
---
+++ 0x00-MySQL_Advanced/README.md
@@ -0,0 +1,7 @@
+# 0x00. MySQL advanced
+
+<img src="https://www.mltut.com/wp-content/uploads/2022/03/Advanced-SQL-Courses.jpg">
+
+## Author
+
+Charjaoui Zakaria - [Github](https://github.com/Zakry27)
